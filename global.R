9d10e6ef1ff0d6cec555b049154a472be56b29f7
ijlyttle <ian.lyttle@schneiderdashelectric.com>
2014-06-25 09:00:32 -0500

    changing to incorporate factor clustering

--- global.R
+++ global.R
@@ -46,13 +46,78 @@ var_names_factor <- var_names[var_names %in% var_factor]
 
 # we want to rename and combine countries
 
+get_linear_coef <- function(model){
+  coef(summary(model))[2, 1]
+}
+
+get_pval <- function(model){
+  coef(summary(model))[2, 4]
+}
+
+get_cluster <- function(linear_coef, pval, sig){
+  
+  cluster <- 
+    ifelse (
+      pval > sig,
+      "not significant",
+      ifelse (
+        linear_coef > 0,
+       "significant positive",
+       "significant negative"
+      )
+    ) 
+      
+  cluster
+}
+
 # we want to group by country, then look at making a linear model WRT Television
-student_models <-
+str_subject <- "PV1MATH"
+str_factor_inner <- "ST27Q02"
+
+student_score_factor <-
   student2012 %>%
-  select(CNT, PV1MATH, ST27Q02) %>%
-  mutate(PV1MATH = as.numeric(PV1MATH)) %>%
-  group_by(CNT) %>%
-  do(model = lm(PV1MATH ~ ST27Q02, data = .))
+  select(
+    country = CNT, 
+    score = get(str_subject), 
+    factor_inner = get(str_factor_inner)
+  ) %>%
+  mutate(
+    score = as.numeric(score),
+    factor_inner = ordered(factor_inner)
+  )   
+  
+student_model <- 
+  student_score_factor %>%
+  group_by(country) %>%
+  do(model = lm(score ~ factor_inner, data = .)) %>%
+  mutate(
+    linear_coef = get_linear_coef(model),
+    pval = get_pval(model),
+    corr = ordered(
+      get_cluster(linear_coef, pval, 0.05), 
+      levels = c("significant negative",
+                 "not significant",
+                 "significant positive")
+    )
+  ) %>%
+  select(-model)
+
+student_summary <- 
+  student_score_factor %>%
+  group_by(country) %>%
+  summarize(median = median(score), count = n()) %>%
+  left_join(student_model, by = "country")
+
+student_summary <-
+  student_summary %>%
+  mutate(country = reorder(country, median))
+
+ggplot(
+  aes(x = median, y = country),
+  data = student_summary
+) +
+  geom_point() +
+  facet_grid(corr ~ ., scales = "free")
 
 # student_results <- 
 #   student_models %>%
